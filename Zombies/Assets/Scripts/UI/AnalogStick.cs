2eedb752b9e9efa2d4063a991ce594221fdb28dc
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2020-03-25 14:45:22 -0300

    fim da aula 1

---
+++ Zombies/Assets/Scripts/UI/AnalogStick.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnalogStick : MonoBehaviour, IDragHandler
+{
+    [SerializeField]
+    private RectTransform BG;
+    [SerializeField]
+    private RectTransform Stick;
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 mousePos = GetMousePosition(eventData);
+        MoveStick(mousePos);
+        Debug.Log(mousePos);
+    }
+
+    private void MoveStick(Vector2 mousePos)
+    {
+        Stick.localPosition = mousePos;
+    }
+
+    private Vector2 GetMousePosition(PointerEventData eventData)
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(BG, eventData.position, eventData.enterEventCamera, out pos);
+        return pos; 
+    }
+}
